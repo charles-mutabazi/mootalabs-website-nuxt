7d6c3c2d97d0d99f0039ac31c43d431f0e015f21
Charles Mutabazi <charles.mutabazi@oneacrefund.org>
2024-09-29 21:30:51 +0200

    work on header navbar

---
+++ components/HeaderNavbar.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+const links = [
+  [{
+    avatar: {
+      src: '~/assets/logo.svg',
+    },
+  }],
+  [
+    {
+      label: 'Home',
+      icon: 'i-heroicons-home',
+      to: '/'
+    },
+    {
+      label: 'About Us',
+      icon: 'i-heroicons-chart-bar',
+      to: '/about'
+    },
+    {
+      label: 'Command Palette',
+      icon: 'i-heroicons-command-line',
+      to: '/components/command-palette'
+    }
+  ]
+]
+</script>
+
+<template>
+  <header>
+    <div class="w-full dark:bg-gray-800 bg-gray-100">
+      <UContainer fluid>
+        <div class="flex justify-between items-center py-2">
+          <UHorizontalNavigation :links="links"/>
+        </div>
+      </UContainer>
+    </div>
+  </header>
+</template>
+
---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <UContainer>
+    <UCard class="mt-10">
+      <template #header>
+        <div class="flex justify-between">
+          <h1>About Us</h1>
+        </div>
+      </template>
+      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation
+      </UButton>
+    </UCard>
+  </UContainer>
+</template>
